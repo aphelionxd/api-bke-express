96b8cd9d72e6d29ffd727ef5a62abafcdb8a282d
aphelionxd <R.batista@aluno.ifsp.edu.br>
2024-08-13 15:33:09 -0300

    second commit for arthur

--- src/server.js
+++ src/server.js
@@ -1,25 +1,14 @@
 import express from 'express'
 import authRouter from './routers/authRouter.js'
+import accountRouter from './routers/accountRouter.js'
 
 const app = express()
 
 app.use('/auth', authRouter)
 
-app.post('/account', (req, res) => {
-    res.json({message: "Rota de POST Account"})
-})
-app.get('/account/list', (req, res) => {
-    res.json({message: "Rota de GET Account/List"})
-})
-app.get('/account/:id', (req, res) => {
-    res.json({message: "Rota de GET Account ID"})
-})
-app.put('/account/:id', (req, res) => {
-    res.json({message: "Rota de GET Account"})
-})
-app.delete('/account/:id', (req, res) => {
-    res.json({message: "Rota de DELETE Account"})
-})
+app.use('/account', accountRouter)
+
+
 app.listen(3000, () => {
     console.log('servidor rodando em http://localhost:3000')
 })
