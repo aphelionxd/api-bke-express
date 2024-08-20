c54bb756571fa28692361f7b820fec603a59ce90
aphelionxd <R.batista@aluno.ifsp.edu.br>
2024-08-20 15:38:08 -0300

    finish routes 2

---
+++ src/models/userModel.js
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export const getAll = async () => {
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            email: true
+        }
+    })
+    return users
+}
+
+export const create = () => {
+
+}
+
--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
+//const express = require('express')
 import express from 'express'
-import authRouter from './routers/authRouter.js'
-import accountRouter from './routers/accountRouter.js'
+import userRouter from './routers/userRouter.js'
+import productRouter from './routers/productRouter.js'
 
 const app = express()
+const port = 3000
 
-app.use('/auth', authRouter)
+app.use('/user', userRouter)
+app.use('/product', productRouter)
 
-app.use('/account', accountRouter)
-
-
-app.listen(3000, () => {
-    console.log('servidor rodando em http://localhost:3000')
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
 })
